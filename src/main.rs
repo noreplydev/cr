1f8f5b7e441e33db902b3f426583c4c87f422435
noreplydev <contact.cristiansanchez@gmail.com>
2023-07-11 23:46:49 +0200

    [FT] Added basic stagged command functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,5 +8,31 @@ fn main() {
 
     if command == "init" {
         fs::create_dir(".cr").unwrap();
+        fs::File::create(".cr/stagged").unwrap();
+    }
+
+    if command == "add" {
+        let dir = fs::read_dir(".");
+
+        let entries = match dir {
+            Ok(entries) => entries,
+            Err(_) => panic!("Error reading directory"),
+        };
+
+        let mut files: Vec<String> = Vec::new();
+
+        for entry in entries {
+            let entry = entry.unwrap();
+            println!("{}", entry.file_name().into_string().unwrap());
+            files.push(entry.file_name().into_string().unwrap());
+        }
+
+        let stagged_files = files.join(",");
+        let write_status = fs::write(".cr/stagged", stagged_files);
+
+        match write_status {
+            Ok(_) => println!("Files added to stagged"),
+            Err(_) => panic!("Error writing stagged file"),
+        }
     }
 }
